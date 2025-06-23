ff4c73fcd38b02f724ae143ab67c55340ab9bd99
Pavel Nikolaev <panikolaev7@gmail.com>
2025-06-23 10:53:34 +0300

    Added pagination

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -6,6 +6,7 @@ class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -27,6 +28,7 @@ class PostFromCategory(ListView):
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
     category = None
+    paginate_by = 5
 
     def get_queryset(self):
         """
