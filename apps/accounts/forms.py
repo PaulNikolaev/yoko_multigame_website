2bd2dd98ed175ac98559b81cae0a70c31f2d2f68
Pavel Nikolaev <panikolaev7@gmail.com>
2025-06-25 12:06:02 +0300

    Added UserRegisterForm, UserLoginForm, UserRegisterView, UserLoginView, UserLogoutView

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -38,15 +39,68 @@ class ProfileUpdateForm(forms.ModelForm):
     Форма обновления данных профиля пользователя
     """
     birth_date = forms.DateField(label='Дата рождения',
-        widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
+                                 widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
     bio = forms.CharField(max_length=500,
                           label='Информация о себе',
                           widget=forms.Textarea(attrs={'rows': 5, "class": "form-control mb-1"}))
 
     avatar = forms.ImageField(label='Аватар', widget=forms.FileInput(attrs={"class": "form-control mb-1"}))
-    country = forms.CharField(label='Страна', max_length=100, widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
+    country = forms.CharField(label='Страна', max_length=100,
+                              widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
     city = forms.CharField(label='Город', max_length=100, widget=forms.TextInput(attrs={"class": "form-control mb-1"}))
 
     class Meta:
         model = Profile
         fields = ('birth_date', 'bio', 'avatar', 'country', 'city')
+
+
+class UserRegisterForm(UserCreationForm):
+    """
+    Переопределенная форма регистрации пользователей
+    """
+
+    class Meta(UserCreationForm.Meta):
+        fields = ('username', 'password1', 'password2', 'email', 'first_name', 'last_name')
+
+    def clean_email(self):
+        """
+        Проверка email на уникальность
+        """
+        email = self.cleaned_data.get('email')
+        if email and User.objects.filter(email=email).exists():
+            raise forms.ValidationError('Такой email уже используется в системе')
+        return email
+
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы регистрации
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs.update({"placeholder": "Придумайте свой логин"})
+        self.fields['password1'].widget.attrs.update({"placeholder": "Придумайте свой пароль"})
+        self.fields['password2'].widget.attrs.update({"placeholder": "Повторите придуманный пароль"})
+        self.fields['email'].widget.attrs.update({"placeholder": "Введите свой email"})
+        self.fields['first_name'].widget.attrs.update({"placeholder": "Ваше имя"})
+        self.fields['last_name'].widget.attrs.update({"placeholder": "Ваша фамилия"})
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({"class": "form-control", "autocomplete": "off"})
+
+
+class UserLoginForm(AuthenticationForm):
+    """
+    Форма авторизации на сайте
+    """
+
+    def __init__(self, *args, **kwargs):
+        """
+        Обновление стилей формы авторизации
+        """
+        super().__init__(*args, **kwargs)
+        self.fields['username'].widget.attrs['placeholder'] = 'Логин пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Пароль пользователя'
+        self.fields['username'].label = 'Логин'
+        for field in self.fields:
+            self.fields[field].widget.attrs.update({
+                'class': 'form-control',
+                'autocomplete': 'off'
+            })
--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
-from .views import ProfileUpdateView, ProfileDetailView
+from .views import (ProfileUpdateView,
+                    ProfileDetailView,
+                    UserRegisterView,
+                    UserLoginView,
+                    UserLogoutView)
 
 
 app_name = 'accounts'
@@ -8,4 +12,7 @@ app_name = 'accounts'
 urlpatterns = [
     path('user/edit/', ProfileUpdateView.as_view(), name='profile_edit'),
     path('user/<slug:slug>/', ProfileDetailView.as_view(), name='profile_detail'),
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('login/', UserLoginView.as_view(), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
