6a192c53e662662a102807d2759e52a070e9b890
Pavel Nikolaev <panikolaev7@gmail.com>
2025-07-17 11:30:29 +0300

    Added test_forms file and defined PostCreateFormTest class

---
+++ apps/blog/tests/test_forms.py
@@ -0,0 +1,38 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.urls import reverse
+
+from ..forms import PostCreateForm
+from ..models import Post, Category, Comment
+
+User = get_user_model()
+
+
+class PostCreateFormTest(TestCase):
+    """
+    Тесты для формы PostCreateForm
+    """
+
+    @classmethod
+    def setUpTestData(cls):
+        """
+        Настройка данных, которые будут использоваться всеми тестовыми методами класса.
+        Выполняется один раз для всего класса.
+        """
+        cls.user = User.objects.create_user(username='testuser', password='password123')
+        cls.category = Category.objects.create(title='Test Category', slug='test-category')
+
+    def test_form_valid_data(self):
+        """
+        Тест: форма должна быть валидна с корректными данными.
+        """
+        form_data = {
+            'title': 'Test Post Title',
+            'category': self.category.pk,
+            'description': 'This is a test post description.',
+            'text': 'This is the full text of the test post.',
+            'status': 'published',
+        }
+        form = PostCreateForm(data=form_data)
+        self.assertTrue(form.is_valid(), f"Form is not valid: {form.errors}")
